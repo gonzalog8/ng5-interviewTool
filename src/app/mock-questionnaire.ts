105458e6683089ba810460e12f07e656f57b8e97
Gonzalo Cadenas <x214216@wnco.com>
2017-12-19 10:04:50 -0600

    Fixed grammar. Added fake mok-questionnaire file.

---
+++ src/app/mock-questionnaire.ts
@@ -0,0 +1,19 @@
+export const MOCKQUESTIONNAIRE: Object[] = [
+    { 'id': 1,
+      'title': 'Object Oriented Programming',
+      'glbAvg' : 4.1,
+      'questions': [
+        { 'id': 1, 'title': 'What is a Constructor?', 'glbAvg' : 4.1 },
+        { 'id': 2, 'title': 'What is an Abstract Class?', 'glbAvg' : 4.3 },
+        { 'id': 3, 'title': 'What is an Interface?', 'glbAvg' : 4.2 },
+      ]
+    },
+    { 'id': 2,
+      'title': 'Spring',
+      'glbAvg' : 3.2,
+      'questions': [
+        { 'id': 4, 'title': 'What is Dependency Injection?', 'glbAvg' : 3.5 },
+        { 'id': 5, 'title': 'What are the types of DI in Spring?', 'glbAvg' : 3.5 }
+      ]
+    }
+];
--- src/app/mock-questions.ts
+++ src/app/mock-questions.ts
@@ -2,8 +2,8 @@ import { Question } from './Question';
 
 export const MOCKQUESTION: Question[] = [
     { 'id': 1, 'topicId': 1, 'title': 'What is a Constructor?', 'glbAvg' : 4.1 },
-    { 'id': 2, 'topicId': 1, 'title': 'What is a Abstract Class?', 'glbAvg' : 4.3 },
-    { 'id': 3, 'topicId': 1, 'title': 'What is a Interface?', 'glbAvg' : 4.2 },
+    { 'id': 2, 'topicId': 1, 'title': 'What is an Abstract Class?', 'glbAvg' : 4.3 },
+    { 'id': 3, 'topicId': 1, 'title': 'What is an Interface?', 'glbAvg' : 4.2 },
     { 'id': 4, 'topicId': 2, 'title': 'What is Dependency Injection?', 'glbAvg' : 3.5 },
     { 'id': 5, 'topicId': 2, 'title': 'What are the types of DI in Spring?', 'glbAvg' : 3.5 }
 ];
